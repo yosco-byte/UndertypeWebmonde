12ce6512d09a8584821d7809bd0cf8f4ae0308c2
yosco-byte <lovesosaoblock06@gmail.com>
2026-07-13 11:37:32 +0200

    Fix formatting and remove redundant lines in GameState

--- GameState.ts
+++ GameState.ts
@@ -1,12 +1,7 @@
 export const gameState = {
   grinchDefeated: false,
   monikaDefeated: false,
-  virusDefeated: false,   
-
-  returnMapId: "ruins_1" as string,
-  returnX: undefined as number | undefined,
-  returnY: undefined as number | undefined,
-};
+  virusDefeated: false,
 
   returnMapId: "ruins_1" as string,
   returnX: undefined as number | undefined,
